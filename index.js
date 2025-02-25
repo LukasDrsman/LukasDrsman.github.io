3452aa4f8241e5140c05be527308fb529c238d5b
LukasDrsman <lukaskodr@gmail.com>
2025-02-25 23:33:42 +0100

    Get JSOON

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const canvas = document.getElementById("interactive-canvas");
 
 window.onload = () => {
 	canvas.innerHTML = "loaded";
+
+	const resp = await fetch(
+		"https://raw.githubusercontent.com/LukasDrsman/LukasDrsman.github.io/refs/heads/main/graph.json", {
+			method: "GET"			
+	});
+
+	canvas.innerHTML += `<br><br>${resp.json}`;
 };
