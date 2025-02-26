fcf61845056852af57f7611d11ceaa3351b0fa43
LukasDrsman <lukaskodr@gmail.com>
2025-02-26 12:03:45 +0100

    Name tags

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const canvas = document.getElementById("interactive-canvas");
 
-const createExperienceBlob = (x, y, tag, canvas) => {
+const createExperienceBlob = (x, y, key, tag, canvas) => {
+	const cont = document.createElement("div")
+	cont.classList.add("blob-container");
+
 	const el = document.createElement("div");
 	el.classList.add("experience-blob");
-	el.style.transform = `translate(${x}px, ${y}px)`;
-	canvas.appendChild(el);
+	
+
+	const nameTag = document.createElement("div");
+	nameTag.classList.add("blob-tag");
+	nameTag.id = `tag@${key}`;
+	nameTag.innerHTML = tag;
+
+	
+	cont.appendChild(el)
+	cont.appendChild(nameTag)
+
+	cont.style.transform = `translate(${x}px, ${y}px)`;
+	canvas.appendChild(cont);
 	return el;
 }
 
@@ -20,15 +34,14 @@ window.onload = async () => {
 	const spheres = data.spheres;
 	const tech = spheres.tech;
 
-	let techUplink = {};
+	let uplink = {};
 	Object.keys(tech).forEach(key => {
-		techUplink[key] = createExperienceBlob(
+		uplink[`tech::${key}`] = createExperienceBlob(
 			Math.floor(Math.random() * visualViewport.width / 2 + (1/3) * visualViewport.width),
 			Math.floor(Math.random() * visualViewport.height / 2),
-			tech[key],
-			canvas
+			key, tech[key], canvas
 		);
 
-		canvas.innerHTML += `<br>k: ${key} v: ${tech[key]}`;
+		canvas.innerHTML += `<br>tag@${key} -> ${tech[key]}`;
 	});
 };
